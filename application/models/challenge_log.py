6a5efe8a1b0f64a861191b6446cd74a8a51d4804
Ben <benmega@gmail.com>
2025-12-08 22:44:06 +0700

    feature: profile update

--- application/models/challenge_log.py
+++ application/models/challenge_log.py
@@ -1,4 +1,3 @@
-# TODO add helper to the remote db
 from application.extensions import db
 
 
--- application/models/skill.py
+++ application/models/skill.py
@@ -5,7 +5,6 @@ Summary: SQLAlchemy model for user skills and tagging.
 """
 
 from sqlalchemy import UniqueConstraint
-
 from application.extensions import db
 
 class Skill(db.Model):
@@ -14,9 +13,13 @@ class Skill(db.Model):
     name = db.Column(db.String(50), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
+    # New Fields for Structured Skills
+    category = db.Column(db.String(50), default='concept') # 'language', 'tool', 'concept'
+    icon = db.Column(db.String(50), default='fas fa-code') # FontAwesome class
+    proficiency = db.Column(db.Integer, default=1) # 1=Bronze, 2=Silver, 3=Gold
+
     # Add a unique constraint for name and user_id
     __table_args__ = (UniqueConstraint('name', 'user_id', name='uq_skill_name_user'),)
 
     def __repr__(self):
-        return f'{self.name}'
-
+        return f'{self.name} ({self.category})'
